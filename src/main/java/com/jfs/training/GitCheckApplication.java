41d8d8630340da1ffe4f856a87f92786e75796d9
LAKKIMSETTI N V S D Ramasubrahmanyam <119732527+Subrahmanyam-lakkimsetti@users.noreply.github.com>
2026-08-24 10:16:54 +0530

    sout added on primary configuration

--- src/main/java/com/jfs/training/GitCheckApplication.java
+++ src/main/java/com/jfs/training/GitCheckApplication.java
@@ -9,11 +9,7 @@ public class GitCheckApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GitCheckApplication.class, args);
 		
-
-		System.out.println("Spring boot application running");
-
-		
-		System.out.println("hello from different branch");
+		System.out.println("Spring boot application started...");
 	}
 
 }
